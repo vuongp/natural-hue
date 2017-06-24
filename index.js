e77f5164eedb48c356cacdd2eab8b898316b0df4
Vuong <h.vuongpham@gmail.com>
2017-06-24 23:00:44 +0200

    Initial version

---
+++ index.js
@@ -0,0 +1,27 @@
+const config = require("./config.json");
+const http = require('http');
+const request = require('request');
+
+function getLightBrightness(lightIndex, callback){
+  request({
+    url: `http://${config.ip}/api/${config.user}/lights/${lightIndex}`,
+    method: 'GET'}, function(err, req, body){
+      callback(JSON.parse(body).state.bri);
+    })
+}
+
+function setLightBrightness(lightIndex, brightness){
+  request({
+    url: `http://${config.ip}/api/${config.user}/lights/${lightIndex}/state`,
+    method: 'PUT',
+    json: {bri: brightness}}, function(err, req, body){})
+}
+
+function getWantedBrightness(){
+  var hour = new Date().getHours();
+  return config.brightness[hour];
+}
+
+setInterval(function(){
+  setLightBrightness(config.light, getWantedBrightness());
+}, 4000);
